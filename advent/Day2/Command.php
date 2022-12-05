546503a0b327f4869d8c2ca5ff16936779ab4515
Heppy <heppy@ekberg.nu>
2022-12-05 17:23:04 +0100

    day 2 - part 1

--- advent/Day2/Command.php
+++ advent/Day2/Command.php
@@ -2,7 +2,9 @@
 
 namespace Advent\Day2;
 
+use Exception;
 use Illuminate\Console\Command as Cmd;
+use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Str;
 
@@ -11,18 +13,80 @@ class Command extends Cmd
     protected $signature = 'day:2';
     protected $description = '';
 
+    const MAP = [
+        'X' => self::ROCK,
+        'Y' => self::PAPER,
+        'Z' => self::SCISSOR,
+        'A' => self::ROCK,
+        'B' => self::PAPER,
+        'C' => self::SCISSOR,
+    ];
+
+    const POINTS = [
+        self::ROCK => 1,
+        self::PAPER => 2,
+        self::SCISSOR => 3,
+    ];
+
+    const PAPER = 'paper';
+    const ROCK = 'rock';
+    const SCISSOR = 'scissor';
+
+    const WINS_AGAINST = [
+        self::ROCK => self::SCISSOR,
+        self::PAPER => self::ROCK,
+        self::SCISSOR => self::PAPER,
+    ];
+
     public function handle()
     {
         $puzzle = File::get(dirname(__FILE__) . '/puzzle');
 
-        $elvesCalories = Str::of($puzzle)->explode(PHP_EOL . PHP_EOL)
-            ->map(fn($elf) => Str::of($elf)->explode(PHP_EOL)->map(fn($calories) => (int) $calories)->sum())
-            ->sortDesc();
+        $games = Str::of($puzzle)->explode(PHP_EOL)
+            ->filter();
 
-        $this->info("Carried calories is: {$elvesCalories->first()}");
+        $part1 = $this->part1($games);
 
-        $top3 = $elvesCalories->take(3)->sum();
+        throw_if($part1 != 10718, new Exception('Part 1 is no longer correct'));
 
-        $this->info("Top 3 carries {$top3} calories together");
+        $this->info("Total score: {$part1}");
+    }
+
+    public function part1(Collection $games)
+    {
+
+        $games = $games->map(function($game) {
+            $choices = explode(' ', $game);
+            $opponent = $this->transform($choices[0]);
+            $mine = $this->transform($choices[1]);
+
+            return [
+                'original' => $game,
+                'mine' => $mine,
+                'opponent' => $opponent,
+            ];
+        });
+
+        $wins = $games->filter(function(array $game) {
+            return $this->wins($game['mine'], $game['opponent']);
+        });
+
+        $draws = $games->filter(function(array $game) {
+            return $game['mine'] === $game['opponent'];
+        });
+
+        $choices = $games->sum(fn($game) => self::POINTS[$game['mine']]);
+
+        return $choices + ($wins->count() * 6) + ($draws->count() * 3);
+    }
+
+    protected function wins($a, $b): bool
+    {
+        return $b == self::WINS_AGAINST[$a];
+    }
+
+    public function transform($choice): string
+    {
+        return self::MAP[$choice];
     }
 }
